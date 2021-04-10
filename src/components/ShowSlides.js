2c2e31f0f0667ba2733a41bc91bcbd36acf60695
Jyothirmai <jyothirmai.kakumanu2711@gmail.com>
2021-04-10 18:07:57 +0530

    committed

--- src/components/ShowSlides.js
+++ src/components/ShowSlides.js
@@ -3,83 +3,37 @@ import React, { useState } from 'react';
 const ShowSlides = ({slides}) => {
 
     console.log("slideTitle"+slides[0].title);
-
-    const [currentSlide,setCurrentSlide] = useState(0);
-    const [nextBtn,setNextBtn] = useState(false);
-    const [prevBtn,setPrevBtn] = useState(true);
-    const [resetBtn,setResetBtn] = useState(true);
-   
-    const getPrevSlide =()=>{
-        console.log("in prev disable");
-        currentSlide >0 && setCurrentSlide(currentSlide - 1);
-        console.log("currentSlide"+currentSlide);
-        if(currentSlide===0){
-            setPrevBtn(true);
-            setResetBtn(true);
-        }else{
-        setPrevBtn(false);
-        setResetBtn(false);
-        }
-        setNextBtn(false);
-        
+    const[index , setindex] = useState(0);
+    const render_content= (indx)=>{
+  
+      return (<> <h1 data-testid="title" className="heading">{slides[indx].title}</h1><p data-testid="text" className="text">{slides[indx].text}</p></>);
+  
     }
-
-    const getNextSlide=()=>{
-        currentSlide < slides.length - 1 && setCurrentSlide(currentSlide + 1);
-        if(currentSlide===slides.length-1){
-            setNextBtn(true);
-            // setResetBtn(true);
-        }else{
-        setNextBtn(false);
-        setResetBtn(false);
-        }
-        setPrevBtn(false);
-        // setResetBtn(false);
-        
+    const handlenext = ()=>{
+      if(index < slides.length -1){
+        setindex(index+1);
+      }
+      
     }
-
-    const resetSlides =()=>{
-        console.log(currentSlide);
-        setCurrentSlide(0);
-        console.log(currentSlide);
-        if(currentSlide===0){
-            console.log("inside reset");
-            setResetBtn(true);
-        }
-        else{
-            console.log("else reset");
-            setResetBtn(false);
-            setPrevBtn(true);
-            setNextBtn(false);
-        }
-
+    const handleprev = ()=>{
+      if(index> 0){
+        setindex(index-1);
+      }
     }
-
+  
+    const handlerestart =()=>{
+      setindex(0);
+    
+  }
     return (
         <div className="carousel">
-            <div className="carouselInner" id="navigation">
-                <div className="left">
-                    <button  data-testid="button-left" disabled ={prevBtn} onClick={getPrevSlide}>
-                        Prev
-                    </button>
-                </div>
-                <div className = "center">
-                    <h1 data-testid="title">{slides[currentSlide].title}</h1>
-                    <p data-testid="text">{slides[currentSlide].text}</p>
-                </div>
-                <div className = "right">
-                    <button data-testid="button-right" disabled={nextBtn} onClick={getNextSlide}>
-                        Next
-                    </button>
-                </div>
-                
-            </div>
-            <div className ="restart">
-                    <button  data-testid="button-restart" disabled={resetBtn} onClick={resetSlides}>
-                        Restart
-                    </button>
-                </div>
-            {/* {display(slides)} */}
+            <div className="poster">
+      {render_content(index)}
+      </div>
+
+      <button data-testid="button-prev" onClick={handleprev} disabled={index==0 ?true:false} >Prev</button>
+      <button data-testid="button-restart"  onClick={handlerestart} disabled={index==0 ?true:false}>Restart</button>
+      <button data-testid="button-next" onClick={handlenext} disabled = {index ==slides.length-1?true:false}>Next</button>
             
         </div>
     );
